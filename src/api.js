426990f940c3292a92d59d2b6e2e3152679b0e18
kwtuku <76758720+kwtuku@users.noreply.github.com>
2021-08-02 21:01:18 +0900

    犬種の選択肢をAPIで取得して表示できるようにした

--- src/api.js
+++ src/api.js
@@ -1,7 +1,15 @@
-export async function fetchImages(breed) {
+export async function fetchImageUrls(breed) {
   const response = await fetch(
     `https://dog.ceo/api/breed/${breed}/images/random/12`
   );
   const data = await response.json();
   return data.message;
+}
+
+export async function fetchBreeds() {
+  const response = await fetch(
+    'https://dog.ceo/api/breeds/list/all'
+  );
+  const data = await response.json();
+  return Object.keys(data.message);
 }
